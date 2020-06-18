8618032a13f273a6223f8e6c9c43574bdf0ddd0e
Eve Reichmann <ereichmann4@aol.com>
2020-06-18 20:36:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,12 @@
+#def square_array(numbers)
+#num = []
+#counter = 0 
+#  while counter < numbers.length 
+#  num.push(numbers[counter] ** 2)
+#  counter += 1 
+#end
+#num
+#end
+
 def square_array(numbers)
-num = []
-counter = 0 
-  while counter < numbers.length 
-  num.push(numbers[counter] ** 2)
-  counter += 1 
-end
-num
-end
+end  
